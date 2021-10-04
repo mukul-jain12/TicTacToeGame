40a07c60d91238ebfb3ef8d649ef3b891102ad3d
Mukul Jain <mukuljain422@gmail.com>
2021-10-04 08:26:46 +0530

    update

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -4,14 +4,17 @@ public class TicTacToeGame {
 	
 	public static void main(String args[]) {
 		
+		//calling UC1 method
 		UC1();
 		
 	}
 	
 	private static void UC1() {
 		
+		//define array
 		char[] board= new char[10];
 		
+		//assign elements in empty array
 		for (int a = 1; a < 10; a++) {
             board[a] = (char)a;
         }
